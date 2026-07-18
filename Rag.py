219856e9cc8b57095b7d6c486bb0d289b5eecb55
Nitish saini <sainnitish488@gmail.com>
2026-07-18 23:00:52 +0530

    Initial RAG chatbot

---
+++ Rag.py
@@ -0,0 +1,86 @@
+import os 
+from dotenv import load_dotenv
+import streamlit as st
+
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain_groq import ChatGroq
+from langchain_classic.chains import RetrievalQA
+# from sentence_transformers import SentenceTransformer
+
+
+class rag_chatbot:
+    def __init__(self):
+        print("loading api")
+        load_dotenv()
+        self.groq_api_key = st.secrets["groq_api_key"]
+
+        if not self.groq_api_key :
+            raise ValueError("key is not found")
+        print("api key fetch secussfuly")
+        self.embedding=HuggingFaceEmbeddings()
+        self.llm=ChatGroq(
+            model="openai/gpt-oss-20b",
+            api_key=self.groq_api_key,
+            temperature=0.5,
+            model_kwargs={'tool_choice':'auto'}
+        )
+    def data_pipline(self,path):
+        print("fetching document")
+
+        loader=PyPDFLoader(path)
+        doc=loader.load()
+        print("document load sucessfuly")
+    
+        
+        if not doc:
+            raise ValueError("document is not available")
+        
+        splitter=RecursiveCharacterTextSplitter(
+            chunk_size=1000,
+            chunk_overlap=200,
+        )
+        chunks=splitter.split_documents(doc)
+
+        print("sucessfully..........")
+
+        # embedding=HuggingFaceEmbeddings()
+        self.vector_store=Chroma.from_documents(
+            documents=chunks,
+            embedding=self.embedding,
+            persist_directory="./chroma_db"
+
+        )
+        self.retriever=self.vector_store.as_retriever(search_kwargs={"k":2})
+        # print("vector store created secussfuly")
+    
+        # llm=ChatGroq(
+        #     model="openai/gpt-oss-20b",
+        #     api_key=self.groq_api_key,
+        #     temperature=0.5,
+        #     model_kwargs={'tool_choice':'auto'}
+        # )
+        self.qa_chain=RetrievalQA.from_chain_type(
+            llm=self.llm,
+            retriever=self.retriever,
+            return_source_documents=True
+
+        )
+    def ask_query(self,query):
+        response=self.qa_chain.invoke({'query':query})
+        return {'result':response['result'],
+                'source':response['source_documents']}
+    
+    
+    
+
+# if __name__=='__main__':
+#     # rag=rag_chatbot()
+#     # docs=rag.load_document('C:\RAG_RANDOM\scholarship_info (1).pdf')
+#     # chunks=rag.splitter(docs)
+#     rag.vector_store(chunks)
+#     rag.llm()
+#     rag.ask_query("who is eligible for scholarship ")
+    
---
+++ app.py
@@ -0,0 +1,45 @@
+import streamlit as st
+from Rag import rag_chatbot
+
+st.title("📄 AI ChatBot")
+
+# Create chatbot only once
+if "rag_chat" not in st.session_state:
+    st.session_state.rag_chat = rag_chatbot()
+
+rag_chat = st.session_state.rag_chat
+
+# uploaded_file = st.file_uploader(
+#     "Upload your PDF",
+#     type=["pdf"]
+# )
+
+# Process PDF only once
+import tempfile
+
+uploaded_file = st.file_uploader("Upload PDF", type="pdf")
+
+if uploaded_file:
+
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".pdf") as tmp:
+
+        tmp.write(uploaded_file.read())
+
+        pdf_path = tmp.name
+
+    rag_chat.data_pipline(pdf_path)
+
+    st.session_state.processed = True
+    st.success("PDF uploaded successfully!")
+
+question = st.text_input("Ask your question")
+
+if st.button("Ask"):
+
+    if question:
+
+        with st.spinner("Thinking..."):
+            answer = rag_chat.ask_query(question)
+
+        st.write(answer['result'])
+        # st.write(answer['source'])
